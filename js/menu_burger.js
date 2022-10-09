01f550005b7a591124a8f6136a75c3c3d6ed72a5
Luvey <82052250+Luvey35@users.noreply.github.com>
2022-10-09 19:42:11 +0200

    Menu burger corrigé

--- js/menu_burger.js
+++ js/menu_burger.js
@@ -1,6 +1,32 @@
 const menuHamburger = document.querySelector(".menu-burger");
-const navLinks = document.querySelector(".nav-links ");
+const navLinks = document.querySelector(".nav-links");
+
+const LinkAcc = document.querySelector(".link-debut");
+const LinkMy = document.querySelector(".link-myself");
+const LinkPro = document.querySelector(".link-projets");
+const LinkSki = document.querySelector(".link-skills");
+const LinkCon = document.querySelector(".link-contact");
 
 menuHamburger.addEventListener('click', ()=> {
+    navLinks.classList.toggle('mobile-menu');
+})
+
+LinkAcc.addEventListener('click', ()=> {
+    navLinks.classList.toggle('mobile-menu');
+})
+
+LinkMy.addEventListener('click', ()=> {
+    navLinks.classList.toggle('mobile-menu');
+})
+
+LinkPro.addEventListener('click', ()=> {
+    navLinks.classList.toggle('mobile-menu');
+})
+
+LinkSki.addEventListener('click', ()=> {
+    navLinks.classList.toggle('mobile-menu');
+})
+
+LinkCon.addEventListener('click', ()=> {
     navLinks.classList.toggle('mobile-menu');
 })
