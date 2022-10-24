1b0fed432dd8f6f85427a449546e6c61cd30c9f9
Luvey <82052250+Luvey35@users.noreply.github.com>
2022-10-24 12:09:22 +0200

    Avancement Compétences

---
+++ js/competences.js
@@ -0,0 +1,7 @@
+let frontWeb = document.querySelector(".Cat-langages-W");
+let softwareDev = document.querySelector(".Cat-langages-D");
+let backWeb = document.querySelector(".Cat-langages-B");
+
+let blockFrontWeb = document.querySelector(".front");
+let blockSoftwareDev = document.querySelector(".software");
+let blockBackWeb = document.querySelector(".back");
