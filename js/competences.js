f4c101e2dcbf88f0c0b04a8501043428a6be20d7
Luvey <82052250+Luvey35@users.noreply.github.com>
2022-10-24 16:22:21 +0200

    Selection des types de langages

--- js/competences.js
+++ js/competences.js
@@ -5,3 +5,34 @@ let backWeb = document.querySelector(".Cat-langages-B");
 let blockFrontWeb = document.querySelector(".front");
 let blockSoftwareDev = document.querySelector(".software");
 let blockBackWeb = document.querySelector(".back");
+
+function getStyle(a, b) {
+    return window.getComputedStyle(b, null)[a];
+}
+
+frontWeb.addEventListener('click', ()=> {
+    if(getStyle('background-color', frontWeb) == "rgb(23, 17, 54)") {}
+    else {
+        frontWeb.classList.toggle('type_selectionne');
+        softwareDev.classList.remove('type_selectionne');
+        backWeb.classList.remove('type_selectionne');
+    }
+})
+
+softwareDev.addEventListener('click', ()=> {
+    if(getStyle('background-color', softwareDev) == "rgb(23, 17, 54)") {}
+    else {
+        softwareDev.classList.toggle('type_selectionne');
+        frontWeb.classList.remove('type_selectionne');
+        backWeb.classList.remove('type_selectionne');
+    }
+})
+
+backWeb.addEventListener('click', ()=> {
+    if(getStyle('background-color', backWeb) == "rgb(23, 17, 54)") {}
+    else {
+        backWeb.classList.toggle('type_selectionne');
+        softwareDev.classList.remove('type_selectionne');
+        frontWeb.classList.remove('type_selectionne');
+    }
+})
