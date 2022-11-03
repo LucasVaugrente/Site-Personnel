278837dbd816dbe69cb1d6e2bfdbb4f79f454605
Luvey <82052250+Luvey35@users.noreply.github.com>
2022-11-03 12:11:47 +0100

    Animation Langages de prog

--- js/reveal_info.js
+++ js/reveal_info.js
@@ -20,7 +20,7 @@ document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
     observer.observe(r);
 })
 
-// ############################################################################ //
+// ############################ PROJETS ################################ //
 
 const handleIntersect2 = function (entries, observer) {
     entries.forEach(function(entry) {
@@ -34,4 +34,20 @@ const handleIntersect2 = function (entries, observer) {
 const observer2 = new IntersectionObserver(handleIntersect2, options);
 document.querySelectorAll('.revelation').forEach(function (r) {
     observer2.observe(r);
+})
+
+// ######################### LANGAGES ############################## //
+
+const handleIntersect3 = function (entries) {
+    entries.forEach(function(entry) {
+        if(entry.intersectionRatio > ratio) {
+            entry.target.classList.add('apparition-visible');
+            observer.observe(entry.target);
+        }
+    });
+}
+
+const observer3 = new IntersectionObserver(handleIntersect3, options);
+document.querySelectorAll('.apparition').forEach(function (r) {
+    observer3.observe(r);
 })
