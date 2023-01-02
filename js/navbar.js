9f46102f0b554e5cc1d672f9e2fc14cb72f28da4
Lucas <lucasvaugrente35@gmail.com>
2023-01-02 20:48:21 +0100

    Propriétés de la navbar changés avec les particules

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', ()=> {
+    if (window.scrollY == 0) {
+        navbar.style.backgroundColor = 'rgba(23, 17, 54, 0)';
+        navbar.style.backdropFilter = 'blur(0px)';
+        navbar.style.transition = "all 0.3s ease";
+        console.log("En haut");
+    } 
+    else if (window.scrollY != 0) {
+        navbar.style.backgroundColor = 'rgba(23, 17, 54, 0.95)';
+        navbar.style.backdropFilter = 'blur(10px)';
+        navbar.style.transition = "all 0.3s ease";
+        console.log("Pu en haut");
+    }
+});
