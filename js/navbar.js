32ddfcd9225b784aef86e2c1316a420521e8c42c
Luvey <82052250+Luvey35@users.noreply.github.com>
2023-01-03 15:15:39 +0100

    Commentaire enlevé js navbar

--- js/navbar.js
+++ js/navbar.js
@@ -5,12 +5,10 @@ window.addEventListener('scroll', ()=> {
         navbar.style.backgroundColor = 'rgba(23, 17, 54, 0)';
         navbar.style.backdropFilter = 'blur(0px)';
         navbar.style.transition = "all 0.3s ease";
-        console.log("En haut");
     } 
     else if (window.scrollY != 0) {
         navbar.style.backgroundColor = 'rgba(23, 17, 54, 0.95)';
         navbar.style.backdropFilter = 'blur(10px)';
         navbar.style.transition = "all 0.3s ease";
-        console.log("Pu en haut");
     }
 });
