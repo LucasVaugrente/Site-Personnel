878d4d81bd5a8c82d91c94a30bf702bc6bf99aeb
Lucas <lucasvaugrente35@gmail.com>
2023-04-08 19:15:25 +0200

    Update Revelations Projets

--- js/reveal_info.js
+++ js/reveal_info.js
@@ -1,36 +1,68 @@
+const ratio = 0.1;
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.1,
+};
+
+// ######################### DEBUT ############################## //
+
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add("reveal-visible");
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
+  observer.observe(r);
+});
+
+// ######################### LANGAGES ############################## //
+
+const handleIntersect3 = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add("apparition-visible");
+      observer.observe(entry.target);
+    }
+  });
+};
+
+const observer3 = new IntersectionObserver(handleIntersect3, options);
+document.querySelectorAll(".apparition").forEach(function (r) {
+  observer3.observe(r);
+});
+
+// ######################### NAVIGATION ############################## //
+
+const handleIntersect4 = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add("nav_reveal-visible");
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const observer4 = new IntersectionObserver(handleIntersect4, options);
+document.querySelectorAll('[class*="nav_reveal"]').forEach(function (r) {
+  observer4.observe(r);
+});
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function myFunction() {
 
-  await sleep(100);
-
-  const ratio = 0.1;
-
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.1,
-  };
-
-  const handleIntersect = function (entries, observer) {
-    entries.forEach(function (entry) {
-      if (entry.intersectionRatio > ratio) {
-        entry.target.classList.add("reveal-visible");
-        observer.unobserve(entry.target);
-      }
-    });
-  };
-
-  const observer = new IntersectionObserver(handleIntersect, options);
-  document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
-    observer.observe(r);
-  });
-
+  await sleep(1);
 
   // ############################ PROJETS ################################ //
-  console.log(document.querySelectorAll('.revelation'));
   const handleIntersect2 = function (entries, observer) {
     entries.forEach(function (entry) {
       if (entry.intersectionRatio > ratio) {
@@ -44,38 +76,6 @@ async function myFunction() {
   document.querySelectorAll('.revelation').forEach(function (r) {
     observer2.observe(r);
   })
-
-  // ######################### LANGAGES ############################## //
-
-  const handleIntersect3 = function (entries, observer) {
-    entries.forEach(function (entry) {
-      if (entry.intersectionRatio > ratio) {
-        entry.target.classList.add("apparition-visible");
-        observer.observe(entry.target);
-      }
-    });
-  };
-
-  const observer3 = new IntersectionObserver(handleIntersect3, options);
-  document.querySelectorAll(".apparition").forEach(function (r) {
-    observer3.observe(r);
-  });
-
-  // ######################### NAVIGATION ############################## //
-
-  const handleIntersect4 = function (entries, observer) {
-    entries.forEach(function (entry) {
-      if (entry.intersectionRatio > ratio) {
-        entry.target.classList.add("nav_reveal-visible");
-        observer.unobserve(entry.target);
-      }
-    });
-  };
-
-  const observer4 = new IntersectionObserver(handleIntersect4, options);
-  document.querySelectorAll('[class*="nav_reveal"]').forEach(function (r) {
-    observer4.observe(r);
-  });
 
 }
 
