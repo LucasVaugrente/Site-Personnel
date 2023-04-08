a589cb71417f5866db07703e0187a086e15fcce2
Lucas <lucasvaugrente35@gmail.com>
2023-04-08 19:13:38 +0200

    Projets Générés dynamiquement (il manque les images responsives)

--- js/projets.js
+++ js/projets.js
@@ -1,179 +1,123 @@
 let Section_Projets = document.querySelector(".section-projets");
 
-let xhr = new XMLHttpRequest();
-
-xhr.open('GET', '../data/projets.json', true);
-
-xhr.setRequestHeader('Content-Type', 'application/json');
-
-xhr.onload = function () {
-    if (xhr.status === 200) {
-        let data = JSON.parse(xhr.responseText);
-
-        // Boucle qui parcourt tous les projets
+fetch('../data/projets.json')
+    .then(response => response.json())
+    .then(data => {
         for (let index = 0; index < data.projets.length; index++) {
 
-            // Projet à gauche
-            if (index % 2 === 0) {
+            let article = document.createElement('article');
+            let bloc_content = document.createElement('div');
+            let bloc_titre = document.createElement('div');
 
-                let article = document.createElement('article');
-                let bloc_content = document.createElement('div');
-                let bloc_titre = document.createElement('div');
+            // Si le projet est à gauche
+            if (index % 2 === 0) {
                 article.classList.add('projet_g');
-                // article.classList.add('revelation');
                 bloc_content.classList.add('p-content-gauche');
                 bloc_titre.classList.add('titre-projet-gauche');
-
-                /* ########################## Date, Type et Titre PROJET ########################## */
-                let Text_Date = document.createElement('p');
-                let Text_Titre = document.createElement('h1');
-                Text_Titre.classList.add('p-titre');
-
-                Text_Date.innerHTML = data.projets[index].date + " - " + data.projets[index].type_projet;
-                Text_Titre.innerHTML = data.projets[index].titre;
-
-                bloc_titre.appendChild(Text_Date);
-                bloc_titre.appendChild(Text_Titre);
-
-                /* ########################## Description PROJET ########################## */
-                let Text_Desc = document.createElement('p');
-                Text_Desc.classList.add('p-pitch-gauche');
-                Text_Desc.innerHTML = data.projets[index].description;
-
-                /* ########################## Langages Programmation PROJET ########################## */
-                let bloc_lan_git = document.createElement('div');
-                bloc_lan_git.classList.add('git-lan');
-
-                for (let j = 0; j < data.projets[index].langages.length; j++) {
-
-                    let ahref = document.createElement('a');
-                    ahref.classList.add("langues-prog");
-                    ahref.innerHTML = data.projets[index].langages[j].nom;
-                    ahref.setAttribute("target", "_blank");
-                    ahref.setAttribute("href", data.projets[index].langages[j].url);
-
-                    bloc_lan_git.appendChild(ahref);
-                }
-
-                /* ########################## Lien GitHub PROJET ########################## */
-                let imgGit = document.createElement("img");
-                let agit = document.createElement("a");
-                agit.classList.add("lien-github_projet");
-
-                imgGit.classList.add("logo-github");
-                imgGit.setAttribute("src", "img/icones/logo/GitHub.png");
-                imgGit.setAttribute("alt", "Logo Github");
-                imgGit.setAttribute("title", "Lien Github");
-                imgGit.setAttribute("width", "30");
-                imgGit.setAttribute("height", "30");
-
-                agit.setAttribute("href", data.projets[index].lien_git);
-                agit.setAttribute("target", "_blank");
-                agit.appendChild(imgGit)
-                bloc_lan_git.appendChild(agit);
-
-                /* ########################## Vidéo PROJET ########################## */
-                let video = document.createElement("video");
-                video.classList.add(data.projets[index].class);
-                // video.setAttribute("src", data.projets[index].video);
-
-                let source = document.createElement("source");
-                source.setAttribute("src", data.projets[index].video);
-                video.appendChild(source);
-
-                video.setAttribute("autoplay", "");
-                video.setAttribute("preload", "auto");
-                video.setAttribute("loop", "");
-                video.setAttribute("muted", "");
-
-                // ##### AJOUT DE TOUS LES ELEMENTS ##### //
-                bloc_content.appendChild(bloc_titre);
-                bloc_content.appendChild(Text_Desc);
-                bloc_content.appendChild(bloc_lan_git);
-                article.appendChild(bloc_content);
-                article.appendChild(video);
-                Section_Projets.appendChild(article);
-            }
-            // Projet à droite
-            else {
-                let article = document.createElement('article');
-                let bloc_content = document.createElement('div');
-                let bloc_titre = document.createElement('div');
+            } else {
                 article.classList.add('projet_d');
-                // article.classList.add('revelation');
                 bloc_content.classList.add('p-content-droite');
                 bloc_titre.classList.add('titre-projet-droite');
-
-                /* ########################## Date, Type et Titre PROJET ########################## */
-                let Text_Date = document.createElement('p');
-                let Text_Titre = document.createElement('h1');
-                Text_Titre.classList.add('p-titre');
-
-                Text_Date.innerHTML = data.projets[index].date + " - " + data.projets[index].type_projet;
-                Text_Titre.innerHTML = data.projets[index].titre;
-
-                bloc_titre.appendChild(Text_Date);
-                bloc_titre.appendChild(Text_Titre);
-
-                /* ########################## Description PROJET ########################## */
-                let Text_Desc = document.createElement('p');
-                Text_Desc.classList.add('p-pitch-droite');
-                Text_Desc.innerHTML = data.projets[index].description;
-
-                /* ########################## Langages Programmation PROJET ########################## */
-                let bloc_lan_git = document.createElement('div');
-                bloc_lan_git.classList.add('git-lan');
-
-                for (let j = 0; j < data.projets[index].langages.length; j++) {
-
-                    let ahref = document.createElement('a');
-                    ahref.classList.add("langues-prog");
-                    ahref.innerHTML = data.projets[index].langages[j].nom;
-                    ahref.setAttribute("target", "_blank");
-                    ahref.setAttribute("href", data.projets[index].langages[j].url);
-
-                    bloc_lan_git.appendChild(ahref);
-                }
-
-                /* ########################## Lien GitHub PROJET ########################## */
-                let imgGit = document.createElement("img");
-                let agit = document.createElement("a");
-                agit.classList.add("lien-github_projet");
-
-                imgGit.classList.add("logo-github");
-                imgGit.setAttribute("src", "img/icones/logo/GitHub.png");
-                imgGit.setAttribute("alt", "Logo Github");
-                imgGit.setAttribute("title", "Lien Github");
-                imgGit.setAttribute("width", "30");
-                imgGit.setAttribute("height", "30");
-
-                agit.setAttribute("href", data.projets[index].lien_git);
-                agit.setAttribute("target", "_blank");
-                agit.appendChild(imgGit)
-                bloc_lan_git.appendChild(agit);
-
-                /* ########################## Vidéo PROJET ########################## */
-                let video = document.createElement("video");
-                video.classList.add(data.projets[index].class);
-                video.setAttribute("src", data.projets[index].video);
-                video.setAttribute("autoplay", "");
-                video.setAttribute("loop", "");
-                video.setAttribute("muted", "");
-                video.setAttribute("preload", "auto");
-
-                // ##### AJOUT DE TOUS LES ELEMENTS ##### //
-                bloc_content.appendChild(bloc_titre);
-                bloc_content.appendChild(Text_Desc);
-                bloc_content.appendChild(bloc_lan_git);
-                article.appendChild(video);
-                article.appendChild(bloc_content);
-                Section_Projets.appendChild(article);
             }
+
+            let p = index + 1;
+            article.classList.add('projet_' + p);
+            // article.classList.add('revelation');
+
+            /* ########################## Date, Type et Titre PROJET ########################## */
+            let Text_Date = document.createElement('p');
+            let Text_Titre = document.createElement('h1');
+            Text_Titre.classList.add('p-titre');
+
+            Text_Date.innerHTML = data.projets[index].date + " - " + data.projets[index].type_projet;
+            Text_Titre.innerHTML = data.projets[index].titre;
+
+            bloc_titre.appendChild(Text_Date);
+            bloc_titre.appendChild(Text_Titre);
+
+            /* ########################## Description PROJET ########################## */
+            let Text_Desc = document.createElement('p');
+
+            // Si le projet est à gauche
+            if (index % 2 === 0) {
+                Text_Desc.classList.add('p-pitch-gauche');
+            } else {
+                Text_Desc.classList.add('p-pitch-droite');
+            }
+
+            Text_Desc.innerHTML = data.projets[index].description;
+
+            /* ########################## Langages Programmation PROJET ########################## */
+            let bloc_lan_git = document.createElement('div');
+            bloc_lan_git.classList.add('git-lan');
+
+            for (let j = 0; j < data.projets[index].langages.length; j++) {
+
+                let ahref = document.createElement('a');
+                ahref.classList.add("langues-prog");
+                ahref.innerHTML = data.projets[index].langages[j].nom;
+                ahref.setAttribute("target", "_blank");
+                ahref.setAttribute("href", data.projets[index].langages[j].url);
+
+                bloc_lan_git.appendChild(ahref);
+            }
+
+            /* ########################## Lien GitHub PROJET ########################## */
+            let imgGit = document.createElement("img");
+            let agit = document.createElement("a");
+            agit.classList.add("lien-github_projet");
+
+            imgGit.classList.add("logo-github");
+            imgGit.setAttribute("src", "img/icones/logo/GitHub.png");
+            imgGit.setAttribute("alt", "Logo Github");
+            imgGit.setAttribute("title", "Lien Github");
+            imgGit.setAttribute("width", "30");
+            imgGit.setAttribute("height", "30");
+
+            agit.setAttribute("href", data.projets[index].lien_git);
+            agit.setAttribute("target", "_blank");
+            agit.appendChild(imgGit)
+            bloc_lan_git.appendChild(agit);
+
+            /* ########################## Vidéo PROJET ########################## */
+            let lien_vid = document.createElement("a");
+            lien_vid.setAttribute("href", data.projets[index].lien_git);
+            lien_vid.setAttribute("target", "_blank");
+
+            lien_vid.classList.add(data.projets[index].class);
+
+            let video = document.createElement("video");
+            let source = document.createElement("source");
+            source.src = data.projets[index].video;
+            source.type = "video/mp4"
+            video.appendChild(source);
+
+            video.setAttribute("controls", "true");
+            video.setAttribute("preload", "metadata");
+            video.setAttribute("loop", "");
+            video.setAttribute("data-video-id", index + 1);
+
+            lien_vid.appendChild(video);
+
+            // ##### AJOUT DE TOUS LES ELEMENTS ##### //
+            bloc_content.appendChild(bloc_titre);
+            bloc_content.appendChild(Text_Desc);
+            bloc_content.appendChild(bloc_lan_git);
+
+            // Si le projet est à gauche
+            if (index % 2 === 0) {
+                article.appendChild(bloc_content);
+                article.appendChild(lien_vid);
+            } else {
+                article.appendChild(lien_vid);
+                article.appendChild(bloc_content);
+            }
+
+            article.classList.add("revelation");
+            Section_Projets.appendChild(article);
+
+
         }
 
-    } else {
-        console.error('Une erreur est survenue');
-    }
-};
-
-xhr.send();
+    })
+    .catch(error => console.error(error));
--- js/reveal_info.js
+++ js/reveal_info.js
@@ -1,69 +1,82 @@
-const ratio = 0.1;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-const options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.1,
-};
+async function myFunction() {
 
-const handleIntersect = function (entries, observer) {
-  entries.forEach(function (entry) {
-    if (entry.intersectionRatio > ratio) {
-      entry.target.classList.add("reveal-visible");
-      observer.unobserve(entry.target);
-    }
+  await sleep(100);
+
+  const ratio = 0.1;
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+
+  const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.intersectionRatio > ratio) {
+        entry.target.classList.add("reveal-visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(handleIntersect, options);
+  document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
+    observer.observe(r);
   });
-};
 
-const observer = new IntersectionObserver(handleIntersect, options);
-document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
-  observer.observe(r);
-});
 
-// ############################ PROJETS ################################ //
+  // ############################ PROJETS ################################ //
+  console.log(document.querySelectorAll('.revelation'));
+  const handleIntersect2 = function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.intersectionRatio > ratio) {
+        entry.target.classList.add('revelation-visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }
 
-const handleIntersect2 = function (entries, observer) {
-  entries.forEach(function (entry) {
-    if (entry.intersectionRatio > ratio) {
-      entry.target.classList.add("revelation-visible");
-      observer.unobserve(entry.target);
-    }
+  const observer2 = new IntersectionObserver(handleIntersect2, options);
+  document.querySelectorAll('.revelation').forEach(function (r) {
+    observer2.observe(r);
+  })
+
+  // ######################### LANGAGES ############################## //
+
+  const handleIntersect3 = function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.intersectionRatio > ratio) {
+        entry.target.classList.add("apparition-visible");
+        observer.observe(entry.target);
+      }
+    });
+  };
+
+  const observer3 = new IntersectionObserver(handleIntersect3, options);
+  document.querySelectorAll(".apparition").forEach(function (r) {
+    observer3.observe(r);
   });
-};
 
-const observer2 = new IntersectionObserver(handleIntersect2, options);
-document.querySelectorAll(".revelation").forEach(function (r) {
-  observer2.observe(r);
-});
+  // ######################### NAVIGATION ############################## //
 
-// ######################### LANGAGES ############################## //
+  const handleIntersect4 = function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.intersectionRatio > ratio) {
+        entry.target.classList.add("nav_reveal-visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  };
 
-const handleIntersect3 = function (entries, observer) {
-  entries.forEach(function (entry) {
-    if (entry.intersectionRatio > ratio) {
-      entry.target.classList.add("apparition-visible");
-      observer.observe(entry.target);
-    }
+  const observer4 = new IntersectionObserver(handleIntersect4, options);
+  document.querySelectorAll('[class*="nav_reveal"]').forEach(function (r) {
+    observer4.observe(r);
   });
-};
 
-const observer3 = new IntersectionObserver(handleIntersect3, options);
-document.querySelectorAll(".apparition").forEach(function (r) {
-  observer3.observe(r);
-});
+}
 
-// ######################### NAVIGATION ############################## //
-
-const handleIntersect4 = function (entries, observer) {
-  entries.forEach(function (entry) {
-    if (entry.intersectionRatio > ratio) {
-      entry.target.classList.add("nav_reveal-visible");
-      observer.unobserve(entry.target);
-    }
-  });
-};
-
-const observer4 = new IntersectionObserver(handleIntersect4, options);
-document.querySelectorAll('[class*="nav_reveal"]').forEach(function (r) {
-  observer4.observe(r);
-});
+myFunction()
