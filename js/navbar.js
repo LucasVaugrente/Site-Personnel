bece82cdb1522b042df6f1f0ebc65e7a0965d95e
lucas <lucas.vaugrente@mobil-inn.com>
2024-06-19 10:55:33 +0200

    Refractor Code CSS/JS

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,55 @@
-let navbar = document.querySelector("nav");
+const navbar = document.querySelector("nav");
+
+const menuBurger = document.querySelector(".menu-burger");
+const menuBurgerClose = document.querySelector(".menu-burger-close");
+const navLinks = document.querySelector(".nav-links");
+
+const LinkAcc = document.querySelector(".link_welcome");
+const LinkMy = document.querySelector(".link-myself");
+const LinkPro = document.querySelector(".link-projects");
+const LinkSki = document.querySelector(".link-skills");
+const LinkExp = document.querySelector(".link-experiences");
+const LinkCon = document.querySelector(".link-contact");
+
+menuBurger.addEventListener("click", () => {
+  navLinks.classList.toggle("open-menu");
+});
+
+menuBurgerClose.addEventListener("click", () => {
+  navLinks.classList.toggle("open-menu");
+});
+
+LinkAcc.addEventListener("click", () => {
+  navLinks.classList.toggle("open-menu");
+});
+
+LinkMy.addEventListener("click", () => {
+  navLinks.classList.toggle("open-menu");
+});
+
+LinkPro.addEventListener("click", () => {
+  navLinks.classList.toggle("open-menu");
+});
+
+LinkSki.addEventListener("click", () => {
+  navLinks.classList.toggle("open-menu");
+});
+
+LinkExp.addEventListener("click", () => {
+  navLinks.classList.toggle("open-menu");
+});
+
+LinkCon.addEventListener("click", () => {
+  navLinks.classList.toggle("open-menu");
+});
+
 
 window.addEventListener("scroll", () => {
   if (window.scrollY == 0) {
+    navbar.classList.add("onTop");
     navbar.classList.remove("notOnTop");
   } else if (window.scrollY != 0) {
+    navbar.classList.remove("onTop");
     navbar.classList.add("notOnTop");
   }
 });
