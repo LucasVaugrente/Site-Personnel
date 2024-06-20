99c3028aaccdfe683dc60be3099f01017388e6c5
lucas <lucas.vaugrente@mobil-inn.com>
2024-06-20 14:58:20 +0200

    Update Button navbar + home

--- js/reveal_info.js
+++ js/reveal_info.js
@@ -12,6 +12,12 @@ const handleIntersectHome = function (entries, observer) {
       entry.target.classList.add("reveal-visible");
       observer.unobserve(entry.target);
     }
+
+    setTimeout(() => {
+      if(entry.target.classList.contains('btn-welcome')) {
+        entry.target.classList.remove("reveal-5");
+      }
+    }, 1000);
   });
 };
 
@@ -26,6 +32,14 @@ const handleIntersectNav = function (entries, observer) {
       entry.target.classList.add("nav_reveal-visible");
       observer.unobserve(entry.target);
     }
+    
+    setTimeout(() => {
+      entry.target.classList.forEach(function (className) {
+        if (className.startsWith('nav_reveal')) {
+          entry.target.classList.remove(className);
+        }
+      });
+    }, 1000);
   });
 };
 
@@ -44,7 +58,6 @@ const handleIntersectContact = function (entries, observer) {
 };
 
 const observerContact = new IntersectionObserver(handleIntersectContact, options);
-console.log(document.querySelectorAll('.revealBlock'));
 document.querySelectorAll('.revealBlock').forEach(function (r) {
   observerContact.observe(r);
 });
